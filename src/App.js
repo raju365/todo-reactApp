1a586e114da634d266af80fe0ea41d2e30a8b487
raju <rajubarman7665@gmail.com>
2023-08-05 11:18:25 +0530

    implimented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
 function App() {
+  const [input, setInput] = useState("");
+  const [listData, setListData] = useState([]);
+
+  function addItems() {
+    setListData([...listData, input]);
+    console.log(listData);
+    setInput("");
+  }
+  function removeList(i) {
+    const updatedListData = listData.filter((e, id) => {
+      return i !== id;
+    });
+    setListData(updatedListData);
+  }
+  function resetList() {
+    setListData([]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input
+            type="text"
+            value={input}
+            placeholder="add a item"
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button onClick={addItems}> + </button>
+          <ol>
+            {listData !== [] &&
+              listData.map((data, i) => {
+                return (
+                  <>
+                    <li key={i}>
+                      {data}
+                      <button onClick={() => removeList(i)}>-</button>
+                    </li>
+                  </>
+                );
+              })}
+            {listData.length >= 1 && (
+              <button className="resetbtn" onClick={resetList}>
+                Reset
+              </button>
+            )}
+          </ol>
+        </div>
+      </div>
+    </>
   );
 }
 
